e8802547b082d9798b6482f0a478cb4852e0a8e4
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2018-10-08 01:22:45 -0500

    cambios pedidos por el profe

--- src/views/asistente.vue
+++ src/views/asistente.vue
@@ -16,208 +16,163 @@
         </ol>
       </div>
       <div class="col-lg-2 p-md">
-        <button v-on:click="patchEjercicio" type="button" class="btn btn-success "><i class="fa fa-upload"></i>&nbsp;&nbsp;
-            <span class="bold">Guardar</span>
+        <button v-on:click="patchEjercicio" type="button" :class="{'btn':true, 'btn-success': !saved, 'btn-primary': saved }">
+          <i v-if="saved" class="fa fa-check"></i>
+          <span v-if="saved" class="bold">Guardado</span>
+          <i v-if="!saved" class="fa fa-upload"></i>
+          <span v-if="!saved" class="bold">Guardar</span>
         </button>
+
       </div>
     </div>
 
     <div class="wrapper wrapper-content animated fadeIn">
-        <!-- <div class="col-lg-12">
-            <span v-html="instruccion"></span>
 
-
-            <button v-on:click="getEjer">Prueba</button>
-        </div> -->
       <div class="row">
         <div class="col-xl-4 col-lg-5  col-md-6">
-            <!-- <span v-html="instruccion"></span>
-
-
-            <button v-on:click="getEjer">Prueba</button> -->
-          <!-- <tinymce id="textEditor" v-if="ajaxFinished" v-bind:content="content" v-on:change="textEdited" ></tinymce> -->
-
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                    <h5>Tarea</h5>
+          <div class="tabs-container">
+            <ul class="nav nav-tabs">
+              <li class="active">
+                <a data-toggle="tab" href="#tab-1" aria-expanded="true">
+  															Ejercicio
+  														</a>
+              </li>
+              <li class="">
+                <a data-toggle="tab" href="#tab-2" aria-expanded="false">
+  															Evaluación
+  														</a>
+              </li>
+            </ul>
+            <div class="tab-content limit-height">
+              <div id="tab-1" class="tab-pane active">
+                <div class="panel-body">
+                  <span v-html="instruccion"></span>
                 </div>
-                <div class="ibox-content">
-                    <span v-html="instruccion"></span>
-                    <div class="row ibox-content">
-                        <h3>Califica el trabajo utilizando la rúbrica</h3>
-                        <!-- <button v-on:click="patchEjercicio">Prueba</button><br> -->
-                        <!-- <tinymce id="textEditor"  v-if="ajaxFinished"  v-bind:readonly="true" user="asistant"  v-bind:content="content" v-on:change="textEdited" ></tinymce> -->
-                        <rubrica v-if="ajaxFinished" type="asistant" v-bind:content="rubrica"  ></rubrica>
-                    </div>
+              </div>
+              <div id="tab-2" class="tab-pane">
+                <div class="panel-body">
+                  <rubrica v-if="ajaxFinished" type="asistant" v-bind:content="rubrica" v-on:change="rubricaEdited"></rubrica>
                 </div>
+              </div>
             </div>
-
+          </div>
         </div>
 
         <div class="col-xl-8 col-lg-7 col-md-6">
           <!-- <rubrica v-if="ajaxFinished" type="asistant" v-bind:content="rubrica"  ></rubrica> -->
           <h3>Comenta el trabajo del estudiante para una mejor retroalimentación</h3>
-          <tinymce id="textEditor"  v-if="ajaxFinished"  v-bind:readonly="true" user="asistant"  v-bind:content="content" v-on:change="textEdited" ></tinymce>
-
-
+          <tinymce id="textEditor" v-if="ajaxFinished" v-bind:readonly="true" user="asistant" v-bind:content="content" v-on:change="textEdited"></tinymce>
         </div>
 
-        <div class="col-lg-12">
-
-        </div>
-      </div>
-
-      <div class="row">
 
       </div>
+
+
 
     </div>
   </div>
 </template>
 
 <script>
-
-// import swal from '../../source_page/js/plugins/sweetalert/sweetalert.min.js'
-import swal from 'sweetalert';
-
-
-/* eslint-disable */
-export default {
-      data(){
-            return {
-                  instruccion: "",
-                  puntos: 0,
-                  content:'static',
-                  rubrica: [],
-                  ajaxFinished: false,
-            }
-      },
-
-      components:{
-            "tinymce" : require('../components/tinymce.vue').default,
-            "rubrica" : require('../components/rubrica.vue').default
-            },
-
-      methods:{
-            textEdited: function (text) {
-                  this.content = text
-            },
-
-            // rubricaEdited: function (text) {
-            //       console.log(text)
-            //       this.rubrica = text
-            // },
-
-            // saveEx : function() {
-            //       var component = this
-            //       // console.log("deleting data")
-            //       //delete previous exercise
-            //       $.ajax({
-            //             type: 'DELETE',
-            //             url: 'http://142.93.52.192:81/ejercicio?id=1',
-            //crossDomain: true,
-            //             complete: function (data) {
-            //                   $.ajax({
-            //                         type: 'POST',
-            //                         url: 'http://142.93.52.192:81/ejercicio',
-            //crossDomain: true,
-            //                         data: {
-            //                               id:1,
-            //                               content: component.content,
-            //                               rubrica: JSON.stringify(component.rubrica)
-            //                               },
-            //                         success: function (data) {
-            //                               // console.log('data posted');
-            //                               swal("Guardado!", "Ejercicio guardado correctamente!", "success");
-            //                         }
-            //                   });
-            //             }
-            //       });
-
-            // },
-
-            getEjer: function(){
-
-
-                var component = this
-                var rubo = []
-                  // console.log($)
-                  $.ajax({
-                        type: "GET",
-                        url: 'http://142.93.52.192:81/ejercicio/1',
-                        crossDomain: true,
-                        success: function (data) {
-
-                            component.content = data.respuesta;
-                            component.instruccion = data.content;
-                            component.rubrica = data.rubrica ? JSON.parse(data.rubrica) : []
-                            // JSON.parse(data.rubrica).forEach(rubro => {
-                            //     // console.log(rubro);
-                            //     rubo.push({pts:rubro.pts,txt:rubro.txt});
-                            // });
-
-                        },
-                        complete:()=>{
-                              component.ajaxFinished = true
-                        }
-
-                  });
-                // this.instruccion = component.content;
-
-            },
-            patchEjercicio: function(){
-
-
-                var component = this
-                // var rubo = []
-                  //console.log(component.content)
-                  $.ajax({
-                        type: "PATCH",
-                        url: 'http://142.93.52.192:81/ejercicio/1',
-                        crossDomain: true,
-                        data: {
-                            // op: "replace",
-                            respuesta: component.content,
-                            rubrica: JSON.stringify(component.rubrica),
-
-
-                        },
-
-                        success: function (data) {
-                            // console.log(data);
-                            swal("Guardado!", "Ejercicio guardado correctamente!", "success");
-
-                        },
-                        complete:()=>{
-                            console.log('Post completado');
-
-                        }
-
-                  });
-            }
-      },
-      computed:{
-        totalPoints: function(){
-            if(this.rubrica.length > 0){
-                var total = 0
-                this.rubrica.forEach(ele => {
-                    total+= ele.value ?  parseInt( ele.pts) : 0
-                });
-                    return total
-            }
-
-            return 0;
-        }
-      },
-
-
-
-      created: async function () {
-            // this.getEjercicio();
-            this.getEjer();
+  /* eslint-disable */
+  export default {
+    data() {
+      return {
+        instruccion: "",
+        puntos: 0,
+        content: 'static',
+        rubrica: [],
+        ajaxFinished: false,
+        saved: false,
       }
+    },
 
-}
+    components: {
+      "tinymce": require('../components/tinymce.vue').default,
+      "rubrica": require('../components/rubrica.vue').default
+    },
+
+    methods: {
+      textEdited: function(text) {
+        this.saved = false
+        this.content = text
+      },
+
+      rubricaEdited: function(text) {
+        this.saved = false
+        this.rubrica = text
+      },
+
+      getEjer: function() {
+
+
+        var component = this
+        var rubo = []
+        // console.log($)
+        $.ajax({
+          type: "GET",
+          url: 'http://142.93.52.192:81/ejercicio/1',
+          crossDomain: true,
+          success: function(data) {
+
+            component.content = data.respuesta;
+            component.instruccion = data.content;
+            component.rubrica = data.rubrica ? JSON.parse(data.rubrica) : []
+          },
+          complete: () => {
+            component.ajaxFinished = true
+          }
+
+        });
+
+      },
+      patchEjercicio: function() {
+        var component = this
+        $.ajax({
+          type: "PATCH",
+          url: 'http://142.93.52.192:81/ejercicio/1',
+          crossDomain: true,
+          data: {
+            // op: "replace",
+            respuesta: component.content,
+            rubrica: JSON.stringify(component.rubrica),
+
+
+          },
+
+          success: function(data) {
+            component.saved = true
+
+          },
+          complete: () => {
+            console.log('Post completado');
+          }
+        });
+      }
+    },
+    computed: {
+      totalPoints: function() {
+        if (this.rubrica.length > 0) {
+          var total = 0
+          this.rubrica.forEach(ele => {
+            total += ele.value ? parseInt(ele.pts) : 0
+          });
+          return total
+        }
+
+        return 0;
+      }
+    },
+    created: async function() {
+      this.getEjer();
+    }
+
+  }
 </script>
+
 <style scoped>
+  .limit-height {
+		height: 70vh;
+		overflow-y: scroll;
+	}
 </style>
--- src/views/student.vue
+++ src/views/student.vue
@@ -1,178 +1,187 @@
 <template>
-  <div>
-    <div class="row wrapper border-bottom white-bg page-heading">
-      <div class="col-lg-10">
-        <h2>Redactar ejercicio</h2>
-        <ol class="breadcrumb">
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a>UI Elements</a>
-          </li>
-          <li class="active">
-            <strong>Panels</strong>
-          </li>
-        </ol>
-      </div>
-      <div class="col-lg-2 p-md">
-        <button v-on:click="patchEjercicio" type="button" class="btn btn-success "><i class="fa fa-upload"></i>&nbsp;&nbsp;
-            <span class="bold">Guardar</span>
-        </button>
-      </div>
-    </div>
+	<div>
+		<div class="row wrapper border-bottom white-bg page-heading">
+			<div class="col-lg-10">
+				<h2>Resolver ejercicio</h2>
+				<ol class="breadcrumb">
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a>UI Elements</a>
+					</li>
+					<li class="active">
+						<strong>Panels</strong>
+					</li>
+				</ol>
+			</div>
+			<div class="col-lg-2 p-md">
+				<button v-on:click="patchEjercicio" type="button" :class="{'btn':true, 'btn-success': !saved, 'btn-primary': saved }">
+									<i v-if="saved" class="fa fa-check"></i>
+									<span v-if="saved" class="bold">Guardado</span>
+									<i v-if="!saved" class="fa fa-upload"></i>
+									<span v-if="!saved" class="bold">Guardar</span>
+								</button>
+			</div>
+		</div>
 
-    <div class="wrapper wrapper-content animated fadeIn">
-        <!-- <div class="col-lg-12">
-            <span v-html="instruccion"></span>
+		<div class="wrapper wrapper-content animated fadeIn">
+
+			<div class="row">
+
+				<div class="col-lg-6">
+					<h3>Redacta tu respuesta</h3>
+					<tinymce id="textEditor" user="student" v-if="ajaxFinished" v-bind:content="content" v-on:change="textEdited"></tinymce>
+				</div>
+
+				<div class="col-lg-6">
+					<div class="tabs-container">
+						<ul class="nav nav-tabs">
+							<li class="active">
+								<a data-toggle="tab" href="#tab-1" aria-expanded="true">
+									Ejercicio
+								</a>
+							</li>
+							<li class="">
+								<a data-toggle="tab" href="#tab-2" aria-expanded="false">
+									Evaluación
+								</a>
+							</li>
+						</ul>
+						<div class="tab-content limit-height">
+							<div id="tab-1" class="tab-pane active">
+								<div class="panel-body">
+                  <span v-html="instruccion"></span>
+								</div>
+							</div>
+							<div id="tab-2" class="tab-pane">
+								<div class="panel-body">
+                  <rubrica v-if="ajaxFinished" type="student" v-bind:content="rubrica"></rubrica>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
 
-            <button v-on:click="getEjer">Prueba</button>
-        </div> -->
-      <div class="row">
-        <div class="col-lg-4">
-            <!-- <span v-html="instruccion"></span>
 
 
-            <button v-on:click="getEjer">Prueba</button> -->
-          <!-- <tinymce id="textEditor" v-if="ajaxFinished" v-bind:content="content" v-on:change="textEdited" ></tinymce> -->
 
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                    <h5>Tarea</h5>
-                </div>
-                <div class="ibox-content">
-                    <span v-html="instruccion"></span>
-                    <div class="row ibox-content">
-                        <!-- <button v-on:click="patchEjercicio">Prueba</button><br> -->
-                        <rubrica v-if="ajaxFinished" type="student" v-bind:content="rubrica" ></rubrica>
-                        <!-- <tinymce id="textEditor" user="student" v-if="ajaxFinished" v-bind:content="content" v-on:change="textEdited" ></tinymce> -->
-                    </div>
-                </div>
-            </div>
+		</div>
 
-        </div>
 
-        <div class="col-lg-8" >
-            <!-- <div class="lol">LOL</div> -->
-        <!-- <div class="lol"> -->
-          <!-- <rubrica v-if="ajaxFinished" type="student" v-bind:content="rubrica" ></rubrica> -->
-            <h3>Redacta tu respuesta</h3>
-            <tinymce id="textEditor" user="student" v-if="ajaxFinished" v-bind:content="content" v-on:change="textEdited" ></tinymce>
 
-        </div>
-
-        <div class="col-lg-12">
-
-        </div>
-      </div>
-
-      <div class="row">
-
-      </div>
-
-    </div>
-  </div>
+	</div>
 </template>
 
 <script>
+	/* eslint-disable */
+	export default {
+		data() {
+			return {
+				instruccion: "",
+				puntos: 0,
+				content: 'static',
+				rubrica: [],
+				ajaxFinished: false,
+				saved: false
+			}
+		},
 
-// import swal from '../../source_page/js/plugins/sweetalert/sweetalert.min.js'
-import swal from 'sweetalert';
+		components: {
+			"tinymce": require('../components/tinymce.vue').default,
+			"rubrica": require('../components/rubrica.vue').default
+		},
 
+		watch: {
+			content: function(content) {
+				this.saved = false
+			}
+		},
 
-/* eslint-disable */
-export default {
-      data(){
-            return {
-                  instruccion: "",
-                  puntos: 0,
-                  content:'static',
-                  rubrica: [],
-                  ajaxFinished: false,
-            }
-      },
+		methods: {
+			textEdited: function(text) {
+				this.saved = false
+				this.content = text
+			},
 
-      components:{
-            "tinymce" : require('../components/tinymce.vue').default,
-            "rubrica" : require('../components/rubrica.vue').default
-            },
-
-      methods:{
-            textEdited: function (text) {
-                  this.content = text
-            },
-
-            getEjer: function(){
-
-
-                var component = this
-                var rubo = []
-                  // console.log($)
-                  $.ajax({
-                        type: "GET",
-                        url: 'http://142.93.52.192:81/ejercicio/1',
-                        crossDomain: true,
-                        success: function (data) {
-
-                            component.content = data.respuesta;
-                            component.instruccion = data.content;
-                            component.rubrica = data.rubrica ? JSON.parse(data.rubrica) : []
+			getEjer: function() {
 
 
-                        },
-                        complete:()=>{
-                              component.ajaxFinished = true
-                        }
+				var component = this
+				var rubo = []
+				// console.log($)
+				$.ajax({
+					type: "GET",
+					url: 'http://142.93.52.192:81/ejercicio/1',
+					crossDomain: true,
+					success: function(data) {
 
-                  });
-
-            },
-            patchEjercicio: function(){
-
-
-                var component = this
-                // var rubo = []
-                  // console.log($)
-                  $.ajax({
-                        type: "PATCH",
-                        url: 'http://142.93.52.192:81/ejercicio/1',
-                        crossDomain: true,
-                        data: {
-                            // op: "replace",
-                            respuesta: component.content,
-                        },
-
-                        success: function (data) {
-                            // console.log(data);
-                            swal("Guardado!", "Ejercicio guardado correctamente!", "success");
-
-                        },
-                        complete:()=>{
-                            console.log('Post completado');
-
-                        }
-
-                  });
-            }
-      },
+						component.content = data.respuesta;
+						component.instruccion = data.content;
+						component.rubrica = data.rubrica ? JSON.parse(data.rubrica) : []
 
 
-      created: async function () {
-            // this.getEjercicio();
-            this.getEjer();
-      }
+					},
+					complete: () => {
+						component.ajaxFinished = true
+					}
 
-}
+				});
+
+			},
+			patchEjercicio: function() {
+
+
+				var component = this
+				// var rubo = []
+				// console.log($)
+				$.ajax({
+					type: "PATCH",
+					url: 'http://142.93.52.192:81/ejercicio/1',
+					crossDomain: true,
+					data: {
+						// op: "replace",
+						respuesta: component.content,
+					},
+
+					success: function(data) {
+						// console.log(data);
+						component.saved = true;
+
+					},
+					complete: () => {
+						console.log('Post completado');
+
+					}
+
+				});
+			}
+		},
+
+
+		created: async function() {
+			// this.getEjercicio();
+			this.getEjer();
+		}
+
+	}
 </script>
-<style scoped>
-    .lol{
-        position: fixed;
-        width: 800px;
-        /* right: 10px; */
-    }
-    .collapse-link{
-        display: none;
 
-    }
+<style scoped>
+	.lol {
+		position: fixed;
+		width: 800px;
+		/* right: 10px; */
+	}
+
+	.limit-height {
+		height: 70vh;
+		overflow-y: scroll;
+	}
+
+	.collapse-link {
+		display: none;
+	}
 </style>
